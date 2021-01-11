bb6b71c3dc11310954c6fc1fb54eb32c49afbe00
GLaDOS <nem35b@protonmail.com>
2021-01-11 16:40:27 -0600

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iomanip>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+/*
+ * So, let's start off by just hardcoding a bunch of shit.
+ *
+ * TODO:
+ * 1. Write a c++ implementation of the bash scripts we are alreadu using to change themes.
+ * 		1a. Bash
+ * 		1b. Vim
+ * 		1c. Alacritty
+ * 		1d. i3
+ *
+ */
+
+int main() {
+
+	std::string root = "/home/smigii/.config/systheme/";
+
+	// BASH
+	// Print out list of bash themes
+	std::string path = root + "programs/bash/bashrc/mcomp-themes/";
+	std::vector<std::string> vec_themes;
+
+	int i = 1;
+	std::cout << "Please enter a theme to use:" << std::endl;
+	for(const auto &entry : fs::directory_iterator(path)){
+		std::string theme_i = entry.path().filename().string();
+		std::cout << i << ". " << theme_i << std::endl;
+		vec_themes.push_back(theme_i);
+		i++;
+	}
+	// Get user to select one
+	std::cout << "Enter a choice between 1 and " << vec_themes.size() << ": ";
+	int choice;
+	while(choice <= 0 || choice > vec_themes.size()){
+		std::cin >> choice;
+	}
+
+	std::cout << "[" << vec_themes.at(choice - 1) << "]" << " selected." << std::endl;
+
+	// Overwrite .bashrc
+	// Create bashrc_temp file.
+	// For this example, follow BASE1 -> THEME -> PROMPT
+	std::fstream file;
+	std::ofstream ofile(root + "programs/bash/bashrc/out", std::ios::out | std::ios::app);
+
+	std::ifstream ifile(root + "programs/bash/bashrc/bcomp-base1");
+	ofile << ifile.rdbuf();
+
+	std::string test = "\n\ntest.\n";
+	ofile << "\n\nHELLO";
+
+
+	return 0;
+}
