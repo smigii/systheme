7d76f0db0fde5382cc0bfdf02a82b04411ca5f51
GLaDOS <nem35b@protonmail.com>
2021-03-23 01:16:15 -0500

    K.I.S.S

--- src/gui/Ui.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by smigii on 2021-01-24.
-//
-
-#include "Ui.h"
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <unistd.h>
 #include <vector>
 #include <string>
 #include <fstream>
@@ -6,104 +7,104 @@
 
 #include <nlohmann/json.hpp>
 
-#include "gui/ui.h"
-#include "driver/Driver.h"
-#include "tpc/Theme.h"
+#define MAX_USER_LEN 16
 
 using json = nlohmann::json;
 namespace fs = std::filesystem;
 
-struct A{
-	int x = 5;
-	int y = 10;
+/*
+ * 1. Get user ~ path
+ * 2. Check if ~/.config/systheme exists
+ *        - If not, prompt to create
+ * 3. Get input...
+ *        - CLA
+ *        - Prompt
+ * 3. Apply
+ */
 
-	void set_x(int i){
-		x = i;
+// === PROTO =============================================================
+
+std::string get_home();
+
+// =======================================================================
+
+int main(int argc, char* argv[]) {
+
+	json json;
+	std::string st_path;
+	std::string th_path;
+	std::string dt_path;
+	std::ifstream ifs_comp;
+	std::ofstream ofs_write;
+
+	st_path = "/home/";
+	st_path += getlogin();
+	st_path += "/.config/systheme/";
+
+	std::cout << st_path << "\n";
+
+	if(!fs::is_directory(st_path)){
+		std::cout << "Please create the ~/.config/systheme directory.\n";
+		return -1;
 	}
 
-	void inc_y(int i){
-		y += i;
+	th_path = st_path + "themes/";
+
+	if(argc == 2){
+		th_path += argv[1];
+		th_path += ".json";
+
+		std::cout << "SELECTED THEME [" << argv[1] << "]\n";
+		std::cout << "PATH [" << th_path << "]\n";
+
+		std::ifstream ifs(th_path);
+		if(ifs.good()){
+			ifs >> json;
+		} else {
+			std::cout << "INVALID FILE\n";
+		}
+	} else {
+		std::cout << "FUCK YOU\n";
+		return -1;
 	}
 
-	void fn(){
-		std::cout << "hello\n";
+
+	std::string p1, p2, p3;
+	for(const auto & prog : json){
+		std::cout << "PROGRAM [" << prog["name"] << "]\n";
+		p1 = st_path + "data/";
+		p1 += prog["name"];
+		p1 += "/";
+
+		for(const auto & config : prog["configs"]){
+			std::cout << "\tCONFIG [" << config["name"] << "]\n";
+
+			p2 = p1;
+			p2 += config["name"];
+			p2 += "/";
+
+			std::string tmp_path = config["dest"];
+			ofs_write.open(tmp_path, std::ios::trunc);
+			ofs_write.close();
+			ofs_write.open(tmp_path, std::ios::out | std::ios::app);
+
+			for(const auto & component : config["components"]){
+				std::cout << "\t\tCOMPONENT [" << component << "]\n";
+
+				p3 = p2;
+				p3 += component;
+
+				std::cout << "\t\t" << p3 << "\n";
+				ifs_comp.open(p3);
+				ofs_write << ifs_comp.rdbuf();
+				ifs_comp.close();
+			}
+			ofs_write.close();
+		}
 	}
-
-	void print(){
-		std::cout << "x : " << x << "\ny : " << y << std::endl;
-	}
-};
-
-struct B{
-	std::vector<void (A::*)()> v;
-	std::vector<void (A::*)(int)> v2;
-};
-
-int test(int x, int y){
-	return x*y;
-}
-
-int main() {
-
-	A a;
-	B b;
-
-	void (A::*fnptr)(){&A::fn};
-	void (A::*fnptr2)(int){&A::set_x};
-
-	b.v.push_back(fnptr);
-	b.v2.push_back(fnptr2);
-
-	(a.*(b.v.at(0)))();
-
-	a.print();
-
-	(a.*(b.v2.at(0)))(69);
-
-	a.print();
-
-//	std::vector<int (*)(int, int)> v;
-//
-//	int (*fnptr)(int, int){ &test };
-//
-//	v.push_back(fnptr);
-//
-//	std::cout << v.at(0)(5, 6) << std::endl;
-
-//	ui();
-
-// ---------------------------------------------------------
-
-//	Driver d;
-//	try {
-//		d.init();
-//	} catch (std::string &e) {
-//		std::cout << e << std::endl;
-//	}
-//	d.apply();
-
-// ---------------------------------------------------------
-
-//	Theme t("Theme 1");
-//
-//	t.add_prog("Bash");
-//	t.at_prog(0)->add_conf("Bashrc", "/home/smigii/.bashrc");
-//	t.at_prog(0)->at_conf(0)->add_comp("Base1");
-//	t.at_prog(0)->at_conf(0)->add_comp("prompt/Minimal");
-//	t.at_prog(0)->at_conf(0)->add_comp("theme/Gruvbox");
-//
-//	t.at_prog(0)->add_conf("Test", "/home/smigii/.test");
-//	t.at_prog(0)->at_conf(1)->add_comp("Base1");
-//	t.at_prog(0)->at_conf(1)->add_comp("prompt/testt");
-//	t.at_prog(0)->at_conf(1)->add_comp("theme/tesssst");
-//
-//	t.add_prog("Vim");
-//	t.at_prog(1)->add_conf(".vimrc", "/home/smigii/.vimrc");
-//	t.at_prog(1)->at_conf(0)->add_comp("Base1");
-//	t.at_prog(1)->at_conf(0)->add_comp("colors/Gruvbox");
-//
-//	std::cout << t << std::endl;
 
 	return 0;
 
 }
+
+// === FUNK ==============================================================
