77df3fb610ca679d71383594a6510301eaa1567a
GLaDOS <nem35b@protonmail.com>
2021-03-26 21:57:41 -0500

    Tidied shit up

--- src/systheme.cpp
+++ src/systheme.cpp
@@ -53,7 +53,7 @@ public:
 	}
 
 	std::string get_st_root(){
-		return root + ".config/systheme/";
+		return (root + ".config/systheme/");
 	}
 
 	std::string handle_rel_path(std::string path){
@@ -67,8 +67,6 @@ public:
 		}
 	}
 
-	in x;
-
 };
 
 class PCC {
@@ -76,16 +74,16 @@ private:
 	std::string prog;
 	std::string conf;
 
-public:
 	void set_prog(std::string _prog) {
 		this->prog = std::move(_prog);
-		conf = nullptr;
+		conf = "";
 	}
 
 	void set_conf(std::string _conf){
 		this->conf = std::move(_conf);
 	}
 
+public:
 	void apply_theme(const json& _json, User usr){
 
 		std::ifstream ifs_comp;
@@ -110,7 +108,7 @@ public:
 				for(const auto & component : config["components"]){
 					std::cout << "\t\tAdding component [" << component << "]...\n";
 
-					std::string path = usr.get_st_root() + "data/" + prog + "/" + conf + "/" + component;
+					std::string path = usr.get_st_root() + "data/" + prog + "/" + conf + "/" + std::string(component);
 
 					ifs_comp.open(path);
 					ofs_write << ifs_comp.rdbuf();
@@ -129,28 +127,22 @@ public:
 int main(int argc, char* argv[]) {
 
 	json json;
-	std::string st_path;
-	std::string th_path;
-	std::string dt_path;
-	std::ifstream ifs_comp;
-	std::ofstream ofs_write;
-
-	User usr = User();
+	User user = User();
+	PCC driver = PCC();
 
 	std::cout << "SYSTHEME 0.0.3\n\n";
 
-	if(!fs::is_directory(usr.get_st_root())){
+	if(!fs::is_directory(user.get_st_root())){
 		std::cout << "Please create the ~/.config/systheme directory.\n";
 		return -1;
 	}
 
 	if(argc == 2){
-		th_path += argv[1];
-		th_path += ".json";
 
 		std::cout << "SELECTED THEME [" << argv[1] << "]\n";
 
-		std::ifstream ifs(usr.get_st_root() + "themes/");
+		std::cout << user.get_st_root() + "themes/" + argv[1] + ".json" << std::endl;
+		std::ifstream ifs(user.get_st_root() + "themes/" + argv[1] + ".json");
 		if(ifs.good()){
 			ifs >> json;
 		} else {
@@ -162,13 +154,15 @@ int main(int argc, char* argv[]) {
 	}
 
 
+	driver.apply_theme(json, user);
+
 	std::cout << "\nDone building theme [" << argv[1] << "]\n";
 
 	// This isn't the WORST thing in the world
 	// since none of the users input ends up
 	// here, but fuck the system()
 	std::cout << "\ncalling refresh.sh\n";
-	std::string refresh_cmd = st_path + "refresh.sh";
+	std::string refresh_cmd = user.get_st_root() + "refresh.sh";
 	system(refresh_cmd.c_str());
 
 	return 0;
